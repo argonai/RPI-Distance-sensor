b1cba96d593d2164f72ca4182ac8def25faae464
Aaron <61741317+argonai@users.noreply.github.com>
2021-01-27 10:47:27 +0100

    IT WORKS

--- main.py
+++ main.py
@@ -2,27 +2,29 @@
 from vlc import Instance
 import os
 import time
-import voorbeeld
 import RPi.GPIO as GPIO
+from random import randint
 GPIO.setmode(GPIO.BOARD)
 
 Trigger_output = 11
 Echo_input = 13
 
 sleeptime = 0.8
-
+random_vid = 0
 GPIO.setup(Trigger_output, GPIO.OUT)
 GPIO.setup(Echo_input, GPIO.IN)
 GPIO.output(Trigger_output, False)
 print("waiting")
+trigger_prevent = 0
+
 class VLC:
     def __init__(self):
-        self.Player = Instance('--loop')
+        self.Player = Instance()
 
     def addPlaylist(self):
         self.mediaList = self.Player.media_list_new()
         #uncoment de lijn 25 en verander de path naar de folder met de gedichten
-        #path = r"path\to\file" 
+        path = r"/home/pi/RPI-Distance-sensor/vids" 
         songs = os.listdir(path)
         for s in songs:
             self.mediaList.add_media(self.Player.media_new(os.path.join(path,s)))
@@ -38,14 +40,18 @@ class VLC:
         self.listPlayer.previous()
     def stop(self):
         self.listPlayer.stop()
-
+player = VLC()
+player.addPlaylist()
+player.play()
+time.sleep(2)
+player.pause()
 try:
     while True:
-    
         GPIO.output(Trigger_output, True)
         time.sleep(0.00001)
         GPIO.output(Trigger_output, False)
         turn_on_time = time.time()
+       
         while GPIO.input(Echo_input) == 0:
             turn_on_time = time.time()
         while GPIO.input(Echo_input) == 1:
@@ -56,11 +62,23 @@ try:
         #distance = time_amount
 
         if(distance < 2 or (round(distance)>200)):
+            print(distance)
             print("Distance out of range")
         else:
             print(f"Distance is: {distance} cm")
         
-        if(round(distance)>80 and round(distance)<120): 
+        
+        if(trigger_prevent == 0 and distance < 130):
+            trigger_prevent = 1
+            player.next()
+        elif(trigger_prevent == 1 and distance > 130):
+            player.pause()
+            trigger_prevent = 0
+       
+            
+        
+            
+       
             
     
     
